6818c98e961d48c8ed0e26317b5d3048d450475f
kanekoNK <72374423+kanekoNK@users.noreply.github.com>
2020-11-26 22:36:56 +0900

    Update myled.c

--- myled/myled.c
+++ myled/myled.c
@@ -11,7 +11,7 @@
 #include <linux/io.h>
 #include <linux/delay.h>
 
-MODULE_AUTHOR("Ryuichi Ueda");
+MODULE_AUTHOR("Naoki Kaneko+Ryuichi Ueda");
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.1");
